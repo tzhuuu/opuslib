74ccece499ff11a4d2481f7f34858bfa1683e4d1
Timo Kösters <timokoesters@protonmail.com>
2018-06-30 08:44:52 +0200

    Update version to 3.0.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ __license__ = 'BSD 3-Clause License'
 
 setuptools.setup(
     name='opuslib',
-    version='3.0.2',
+    version='3.0.3',
     author='Никита Кузнецов',
     author_email='self@svartalf.info',
     maintainer='Orion Labs, Inc.',
