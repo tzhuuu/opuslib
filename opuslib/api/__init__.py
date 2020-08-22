d59332b043ddc11d99dc6d7d93b0744cd28c30c5
tzhuuu <tzhu65@gmail.com>
2020-08-22 15:10:00 -0700

    Update __init__.py

--- opuslib/api/__init__.py
+++ opuslib/api/__init__.py
@@ -6,21 +6,40 @@
 """OpusLib Package."""
 
 import ctypes  # type: ignore
-
+import os
+import sys
 from ctypes.util import find_library  # type: ignore
 
 __author__ = 'Никита Кузнецов <self@svartalf.info>'
 __copyright__ = 'Copyright (c) 2012, SvartalF'
 __license__ = 'BSD 3-Clause License'
 
+libopus = None
 
-lib_location = find_library('opus')
 
-if lib_location is None:
-    raise Exception(
-        'Could not find Opus library. Make sure it is installed.')
+def _load_default():
+    global libopus
+    if sys.platform == 'win32':
+        _basedir = os.path.dirname(os.path.abspath(__file__))
+        _bitness = 'x64' if sys.maxsize > 2 ** 32 else 'x86'
+        _filename = os.path.join(_basedir, 'bin', 'libopus-0.{}.dll'.format(_bitness))
+        libopus = _libopus_loader(_filename)
+    else:
+        lib_location = find_library('opus')
+        if lib_location is None:
+            raise Exception(
+                'Could not find Opus library. Make sure it is installed.')
 
-libopus = ctypes.CDLL(lib_location)
+        libopus = _libopus_loader(lib_location)
+
+
+def _libopus_loader(name):
+    # create the library...
+    lib = ctypes.cdll.LoadLibrary(name)
+    return lib
+
+
+_load_default()
 
 c_int_pointer = ctypes.POINTER(ctypes.c_int)
 c_int16_pointer = ctypes.POINTER(ctypes.c_int16)
